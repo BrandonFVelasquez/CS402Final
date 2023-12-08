ce4316d01f3f0bf3032f49f774332e9e86e5131c
Tanner Collins <tannercollins946@u.boisestate.edu>
2023-12-08 14:15:26 -0700

    fixed deleting markers

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef, useCallback} from 'react';
 import {
   VirtualizedList,
   TouchableOpacity,
@@ -6,13 +6,12 @@ import {
   StyleSheet,
   Text,
   View,
+  Platform,
   useWindowDimensions,
 } from 'react-native';
 import MapView from 'react-native-maps';
 import {Marker} from 'react-native-maps';
-import DialogInput from 'react-native-dialog-input';
 import Geocoder from 'react-native-geocoding';
-import * as Location from 'expo-location';
 import * as ScreenOrientation from 'expo-screen-orientation';
 // create a style sheet for handling visual appearances, spacing, widths, and colors
 const styles = StyleSheet.create({
@@ -21,14 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     paddingTop: 50,
-  },
-  bcontainer: {
-    flexDirection: "row",
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: 50,
-    width: '100%'
   },
   list: {
     flex: 20,
@@ -67,92 +58,9 @@ const Item = ({ item, onPress, backgroundColor, textColor }) => (
     <Text style={[styles.title, textColor]}>{item.key}</Text>
   </TouchableOpacity>
 );
-//adds items from a json file to alist given: 
-  //aurl (the url of the json file to load from)
-  //alist (the list to add to)
-  //asetlist (the setter function for alist)
-  //asetmarkers (the setter function for the markers list [not given])
-async function loadList(aurl,alist,asetlist,asetmarkers) {
-  const response = await fetch(aurl);  // read the remote data file via fetch 'await' blocks
-  const items = await response.json(); // parse the returned json object
-  
-  // add the returned list to the existing list
-  items.forEach((item) => {
-     alist.push(item);
-  })
-  
-  // creates of copy of the modified alist for asetlist
-  const newList = alist.map((item) => {return item});
-  
-  // creates a list of markers from alist
-  const newMarkerList = alist.map((item) => {
-    var newMarker =  <Marker
-      coordinate={{latitude: item.latitude, longitude: item.longitude}}
-      title={item.key}
-      description={"Place"}
-      onPress={(event) => {
-      const markerId = event.nativeEvent.id;
-      console.log('Another marker clicked', markerId);
-      //adding functionality to remove marker when clicked
-      removeMarker(item, alist, asetmarkers);
-    }}
-    />;
 
-    return newMarker;
-  });
-  //sets the newList and newMarkerList
-  asetlist(newList);
-  asetmarkers(newMarkerList);
 
-}
 
-//removes marker when the marker is pressed
-function removeMarker(theMarkerToRemove, markerList, markerSetter){
-  console.log("Marker being removed...\n");
-  const filteredMarkers = markerList.filter(marker => marker.key !== theMarkerToRemove.key);
-  markerSetter(filteredMarkers);
-  console.log("Marker has been elimated!\n")
-}
-
-//saves alist as a json at aurl given:
-  //aurl (the url for the json to be saved to)
-  //alist (the list to be saved)
-async function saveList(aurl, alist) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(alist)
-    };
-    await fetch(aurl, requestOptions);
-}
-//adds the user's current location to the list and marker list given:
-  //asetlist (setter for the list [not given])
-  //asetmarkers (setter for the markers list [not given])
-async function addMyLocation(alist, amarkerlist, asetlist, asetmarkers) {
-  let {status}  = await Location.requestForegroundPermissionsAsync();
-   
-  if (status !== 'granted') {
-    asetlist( 'Permission to access location was denied');
-  }
-  let location = await Location.getCurrentPositionAsync({});
-  var newList = [{
-    key: "My Position", 
-    selected: false, 
-    latitude: location.coords.latitude,
-    longitude: location.coords.longitude
-  }];
-  var newMarkerList = [<Marker
-    coordinate={{latitude: location.coords.latitude,
-    longitude: location.coords.longitude}}
-    title={"My Position"}
-    description={"Place"}
-  />];
-  
-  newList = newList.concat(alist);
-  newMarkerList = newMarkerList.concat(amarkerlist);
-  asetlist(newList);
-  asetmarkers(newMarkerList);
-}
 //data to populate the map before adding geocoder
 //const someList = [{key: "boise", selected: false, latitude: 43.618881, longitude: -116.215019}];
 //const someMarker = [<Marker coordinate={{latitude: 43.618881,longitude: -116.215019}} title={"boise"} description={"city"} />];
@@ -163,56 +71,58 @@ async function lockOrientation() {
   await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
 }
 
-var markSpawnSpeed = 5000;
+var markSpawnSpeed = 2000;
 
 const VirtualList = () => {
   const [list, setlist] = useState([]);
-  const [showAddMenu,setAddMenu] = useState(false);
   const [markers, setmarkers] = useState([]);
   const [clickCount, setClickCount] = useState(0);
   const [levelPos, setLevelPos] = useState({lat: 0.0, long: 0.0});
   const timeoutRef = useRef(null);
-  
-  //loads the list from a json url at start up
+
   useEffect(() => {
-    // var urladdress = "https://cs.boisestate.edu/~scutchin/cs402/codesnips/loadjson.php?user=tannerco";
-    // loadList(urladdress, list, setlist, setmarkers);
     lockOrientation();
     timeoutRef.current = setTimeout(gmark, markSpawnSpeed);
-    return () => {clearTimeout(timeoutRef.current); console.log("reset");} 
+    return () => {
+      clearTimeout(timeoutRef.current);
+    }; 
   }, [levelPos, markers]);
+
+  const removeMarker = (markerKey) => {
+    setmarkers((prevMarkers) => {
+      const updatedMarkers = prevMarkers.filter((marker) => marker.key != markerKey);
+      return updatedMarkers;
+    });
+  };
+
+  const createMarker = (lat, long) => {
+    const newMarker = {
+      key: Date.now(),
+      coordinate: {latitude: lat, longitude: long},
+    };
+    return (
+      <Marker
+        key={newMarker.key}
+        coordinate={newMarker.coordinate}
+        onPress={() => removeMarker(newMarker.key)} 
+      />
+    );
+  };
 
   function generateMarker(lat, long) {
     markLat = Math.random()*0.08 + lat - 0.05;
     markLong = Math.random()*0.08 + long - 0.05;
-    let newMarkerList = [
-      <Marker
-        coordinate={{latitude: markLat, longitude: markLong}}
-        description={"Place"}
-        key={Math.random}
-        onPress={(event) => {
-          const markerId = event.nativeEvent.id;
-          removeMarker(this.key, markers, setmarkers);
-        }}
-      />
-    ]
-    newMarkerList = newMarkerList.concat(markers);
-    setTheMarkers(newMarkerList);
-    //setmarkers(newMarkerList);
-    console.log(timeoutRef.current + ", " + lat + ", " + long);
+    let mark = createMarker(markLat, markLong);
+    setmarkers((prevMarkers) => [mark, ...prevMarkers]);
   }
 
   function gmark() {
     generateMarker(levelPos.lat, levelPos.long);
-    console.log(levelPos.lat);
-  }
-
-  function setTheMarkers(mList) {
-    setmarkers(mList);
+    timeoutRef.current = setTimeout(gmark, markSpawnSpeed);
   }
   
-  const handleButtonClick = () => {
-    setClickCount(clickCount + 1);
+  const incrementClickCounter = () => {
+    setClickCount((previous) => (previous + 1));
     //We may need to add more logic for the clicker
   };
   
@@ -234,58 +144,7 @@ const VirtualList = () => {
     });
     setlist(newList);
   }
-  //displays the add location DialogInput box when the '+' button is pressed
-  function plusButton() {
-    setAddMenu(true);
-  }
-  function plusMyLocationButton() {
-    addMyLocation(list, markers, setlist, setmarkers);
-  }
-  //adds a location to the list based on the first result returned by Geocoder given alocation
-  function addLocation(alocation) {  
-    var location = {};
-    
-    Geocoder.from(alocation)
-    .then(json => {
-      location = json.results[0].geometry.location;
-      
-      var newList = [{key: alocation, selected: false, longitude: location.lng, latitude: location.lat }]
-      var amark = <Marker
-        coordinate={{latitude: location.lat, longitude: location.lng}}
-        title={alocation}
-        description={"Place"}
-      />
-      newList = newList.concat(list);
-      var marklist = markers.concat(amark);
-      setlist(newList);
-      setmarkers(marklist);
-    })
-    .catch(error => console.warn(error));
-  }
-  //removes all selected items from the list when the '-' button is pressed
-  function minusButton() {
-    const newList = list.filter((item) => item.selected == false);
-    const newMarkerList = newList.map((item) => {
-      var newMarker =  <Marker
-        coordinate={{latitude: item.latitude, longitude: item.longitude}}
-        title={item.key}
-        description={"Place"}
-      />;
-      return newMarker;
-    });
-    setlist(newList);
-    setmarkers(newMarkerList);
-  }
-  //adds the items from the json ruturned by the php url to the list when the 'load' button is pressed
-  function loadButton() {
-      var urladdress = "https://cs.boisestate.edu/~scutchin/cs402/codesnips/loadjson.php?user=tannerco"
-      loadList(urladdress, list, setlist, setmarkers);
-  }
-  //saves the items from the list as a json file using the php url when the 'save' button is pressed
-  function saveButton() {
-      var urladdress = "https://cs.boisestate.edu/~scutchin/cs402/codesnips/savejson.php?user=tannerco"
-      saveList(urladdress,list)
-  }
+
   //used by the virtual list to render the list items
   const renderItem = ({ item, index }) => {
     const backgroundColor = item.selected ? 'black' : 'white';
@@ -308,8 +167,8 @@ const VirtualList = () => {
       const response = await Geocoder.from(locationName);
       const { lat, lng } = response.results[0].geometry.location;
       mapref.current.animateToRegion({
-        latitude: lat,
-        longitude: lng,
+        latitude: Platform.OS === "ios" ? lat + 0.05 : lat,
+        longitude: Platform.OS === "ios" ? lng - 0.05 : lng,
         latitudeDelta: 0.1,
         longitudeDelta: 0.1,
       });
@@ -365,8 +224,6 @@ var alist = (
               onPress={() => navigateToLocation(level.location)}
             />
           ))}
-        <Button title="Load" onPress={() => loadButton()} />
-        <Button title="Save" onPress={() => saveButton()} />
       </View>
     </View>
     <Text>Click Counter: {clickCount}</Text>            
@@ -378,55 +235,9 @@ var alist = (
       getItemCount={(data) => list.length}
       getItem={(data, index) => list[index]}
     />
-    <DialogInput
-      isDialogVisible={showAddMenu}
-      title="Enter Address"
-      message="Enter The Address To Add"
-      submitInput={(inputText) => {
-        setAddMenu(false);
-        addLocation(inputText);
-      }}
-      closeDialog={() => {
-        setAddMenu(false);
-      }}
-    >
-      <Text>Something</Text>
-    </DialogInput>
   </View>
 );
-  // //the content to be displayed on the screen when the screen is in landscape mode
-  // var blist = (
-  //   <View style={styles.bcontainer}>
-  //     <View>
-  //       <View style={styles.rowblock}>
-  //         <View style={styles.buttonContainer}>
-  //           <Button title="+" onPress={() => plusButton()} />
-  //           <Button title="-" onPress={() => minusButton()} />
-  //           <Button title="Load" onPress={() => loadButton()} />
-  //           <Button title="Save" onPress={() => saveButton()} />
-  //         </View>
-  //       </View>
-  //         <VirtualizedList 
-  //           style={styles.list}
-  //           data={[]} 
-  //           renderItem={renderItem}
-  //           keyExtractor={(item,index) => index} 
-  //           getItemCount={(data) => list.length} 
-  //           getItem={(data, index) => {return list[index]}} 
-  //         />
-  //         <DialogInput isDialogVisible={showAddMenu} 
-  //           title="Enter Address"
-  //           message="Enter The Address To Add"
-  //           submitInput={ (inputText) => {setAddMenu(false); addLocation(inputText)}}
-  //           closeDialog={() => {setAddMenu(false)}}
-  //         >
-  //         <Text>Something</Text>
-  //         </DialogInput>
-  //       </View>
-  //     {mymap}
-  //   </View>
-  // );
-  //determines the layout base on if the screen is in landscape or portrait mode
+
   return alist;
 };
 export default VirtualList;
